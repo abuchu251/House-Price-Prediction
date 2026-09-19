920b4c456f591224038b755376506276423f146b
Samuel Kendie <samuelkendie@gmail.com>
2026-09-19 09:02:12 +0300

    Add requirements.txt for project dependencies
    - Included essential libraries: numpy, pandas, matplotlib, seaborn, ipykernel, scikit-learn, scipy, and streamlit.

---
+++ app/app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import joblib
+from xgboost import XGBRegressor
+model = joblib.load(r'C:\Users\Kendie\Desktop\projects\ml projects\house price prediction\models\house_price_model.pkl')
+
+import sys
+st.write(sys.executable)
+
+st.title('House Price Prediction')
+import streamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load("house_price_model.pkl")
+
+st.title("House Price Prediction")
+
+overall_qual = st.slider("Overall Quality", 1, 10, 5)
+gr_liv_area = st.number_input("Living Area (sq ft)", 500, 10000, 1500)
+garage_cars = st.slider("Garage Cars", 0, 5, 2)
+
+if st.button("Predict"):
+    
+    data = pd.DataFrame({
+        "OverallQual": [overall_qual],
+        "GrLivArea": [gr_liv_area],
+        "GarageCars": [garage_cars]
+    })
+    
+    prediction = model.predict(data)[0]
+    
+    st.success(f"Predicted Price: ${prediction:,.2f}")
